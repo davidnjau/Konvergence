fd8e42ace6b48932d088a15ef7fff06da4360ec8
Dave <davidnjau21@gmail.com>
2022-07-26 23:12:26 +0300

    feat(core): add new role

--- src/main/java/com/dave/konvergenz/KonvergenzApplication.java
+++ src/main/java/com/dave/konvergenz/KonvergenzApplication.java
@@ -1,13 +1,38 @@
 package com.dave.konvergenz;
 
+import com.dave.konvergenz.backend.authentication.entity.Role;
+import com.dave.konvergenz.backend.authentication.service_class.impl.StaffDetailsServiceImpl;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class KonvergenzApplication {
+public class KonvergenzApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(KonvergenzApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner runner(StaffDetailsServiceImpl staffDetailsService){
+        return args -> {
+            staffDetailsService.saveRoles(new Role( "ROLE_ADMIN"));
+            staffDetailsService.saveRoles(new Role( "ROLE_USER"));
+            staffDetailsService.saveRoles(new Role( "ROLE_HR"));
+        };
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
